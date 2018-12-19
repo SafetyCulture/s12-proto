dc24aeb463ea2b94213cfb4a7aa83d769ef0f246
Arkady Balaba <arkady.balaba@safetyculture.io>
2018-12-19 13:46:06 +1100

    Payload logging in the APIs/gRPC interceptor

--- protobuf/protoc-gen-logger/example/example.logger.pb.go
+++ protobuf/protoc-gen-logger/example/example.logger.pb.go
@@ -15,10 +15,7 @@ var _ = proto.Marshal
 var _ = fmt.Errorf
 var _ = math.Inf
 
-func (this *ExampleMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) *ExampleMessage {
-	type foo interface {
-		Parse(func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) proto.Message
-	}
+func (this *ExampleMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) proto.Message {
 	res := &ExampleMessage{}
 	if isLevelEnabled(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level_DEBUG) {
 		res.Id = this.Id
@@ -27,31 +24,25 @@ func (this *ExampleMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s
 	if isLevelEnabled(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level_ERROR) {
 		res.Password = this.Password
 	}
-	res.InnerMessage = this.InnerMessage.Parse(isLevelEnabled)
+	res.InnerMessage = this.InnerMessage.Parse(isLevelEnabled).(*InnerMessage)
 	if reflect.TypeOf(this.TestOneof) == reflect.TypeOf(&ExampleMessage_OneOf1{}) {
 		res.TestOneof = this.TestOneof
 	}
 	if reflect.TypeOf(this.TestOneof) == reflect.TypeOf(&ExampleMessage_OneOf2{}) {
 		if isLevelEnabled(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level_ERROR) {
 			oneof_ExampleMessage_OneOf2 := this.TestOneof.(*ExampleMessage_OneOf2)
-			oneof_ExampleMessage_OneOf2.OneOf2 = oneof_ExampleMessage_OneOf2.OneOf2.Parse(isLevelEnabled)
+			oneof_ExampleMessage_OneOf2.OneOf2 = oneof_ExampleMessage_OneOf2.OneOf2.Parse(isLevelEnabled).(*OneOfMessage)
 			res.TestOneof = oneof_ExampleMessage_OneOf2
 		}
 	}
 	return res
 }
-func (this *OneOfMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) *OneOfMessage {
-	type foo interface {
-		Parse(func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) proto.Message
-	}
+func (this *OneOfMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) proto.Message {
 	res := &OneOfMessage{}
 	res.Value = this.Value
 	return res
 }
-func (this *InnerMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) *InnerMessage {
-	type foo interface {
-		Parse(func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) proto.Message
-	}
+func (this *InnerMessage) Parse(isLevelEnabled func(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level) bool) proto.Message {
 	res := &InnerMessage{}
 	if isLevelEnabled(github_com_SafetyCulture_s12_proto_protobuf_s12proto.Level_DEBUG) {
 		res.Body = this.Body
