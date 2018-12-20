06ebf22670a1318ed575571177bbf70fbfed9b25
Arkady Balaba <34493923+arkadysafety@users.noreply.github.com>
2018-12-20 13:23:18 +1100

    Payload logging in the APIs/gRPC interceptor (#11)

--- protobuf/protoc-gen-logger/plugin/plugin.go
+++ protobuf/protoc-gen-logger/plugin/plugin.go
@@ -1,9 +1,6 @@
 package plugin
 
 import (
-	"fmt"
-	"strings"
-
 	logger "github.com/SafetyCulture/s12-proto/protobuf/s12proto"
 	"github.com/gogo/protobuf/proto"
 	"github.com/gogo/protobuf/protoc-gen-gogo/descriptor"
@@ -14,10 +11,10 @@ type plugin struct {
 	*generator.Generator
 	generator.PluginImports
 
-	fmtPkg    generator.Single
-	logrusPkg generator.Single
-	s12proto  generator.Single
-	refectPkg generator.Single
+	reflectPkg generator.Single
+	fmtPkg     generator.Single
+	logrusPkg  generator.Single
+	s12proto   generator.Single
 }
 
 func New() generator.Plugin {
@@ -35,7 +32,7 @@ func (p *plugin) Init(g *generator.Generator) {
 func (p *plugin) Generate(file *generator.FileDescriptor) {
 	p.PluginImports = generator.NewPluginImports(p.Generator)
 	p.fmtPkg = p.NewImport("fmt")
-	p.refectPkg = p.NewImport("reflect")
+	p.reflectPkg = p.NewImport("reflect")
 	p.logrusPkg = p.NewImport("github.com/sirupsen/logrus")
 	p.s12proto = p.NewImport("github.com/SafetyCulture/s12-proto/protobuf/s12proto")
 
@@ -50,109 +47,48 @@ func (p *plugin) generateParseFunction(file *generator.FileDescriptor, message *
 	p.P(`func (this *`, ccTypeName, `) Parse(isLevelEnabled func(`, p.s12proto.Use(), `.Level) bool) proto.Message {`)
 	p.In()
 
-	p.P(`type foo interface{`)
-	p.In()
-	p.P()
-	p.P(`Parse(func(`, p.s12proto.Use(), `.Level) bool) proto.Message`)
-	p.Out()
-	p.P(`}`)
-
-	oneOfsMap := make(map[string]string)
 	p.P(`res:=&`, ccTypeName, `{}`)
 	for _, field := range message.Field {
 		var (
-			fieldNameOneOf = p.GetOneOfFieldName(message, field)
 			fieldName      = p.GetFieldName(message, field)
+			fieldNameOneOf = p.GetOneOfFieldName(message, field)
 		)
 
-		if fieldName != fieldNameOneOf { // this is oneOf field
-
-			p.generateOneOfFieldRow(ccTypeName, fieldName, fieldNameOneOf, file.GoPackageName(), field)
-			oneOfsMap[fieldName] = fieldName
+		if fieldName != fieldNameOneOf {
+			if !hasPayloadLoggerExtensions(field) {
+				p.P(`if `, p.reflectPkg.Use(), `.TypeOf(this.`, fieldName, `) == `, p.reflectPkg.Use(), `.TypeOf(&`, ccTypeName, `_`, fieldNameOneOf, `{}){`)
+				p.In()
+				p.P(`res.`, fieldName, `=this.`, fieldName)
+				p.Out()
+				p.P(`}`)
+			} else {
+				p.P(`if isLevelEnabled(`, p.s12proto.Use(), `.Level_`, getLevelValue(field).String(), `) {`)
+				p.In()
+				p.P(`if `, p.reflectPkg.Use(), `.TypeOf(this.`, fieldName, `) == `, p.reflectPkg.Use(), `.TypeOf(&`, ccTypeName, `_`, fieldNameOneOf, `{}){`)
+				p.In()
+				p.P(`res.`, fieldName, `=this.`, fieldName)
+				p.Out()
+				p.P(`}`)
+				p.Out()
+				p.P(`}`)
+			}
 			continue
 		}
 
 		if !hasPayloadLoggerExtensions(field) {
-			p.generateParseCallRow(field, fieldName, file.GoPackageName())
-
+			p.P(`res.`, fieldName, `=this.`, fieldName)
 			continue
+		} else {
+			p.P(`if isLevelEnabled(`, p.s12proto.Use(), `.Level_`, getLevelValue(field).String(), `) {`)
+			p.In()
+			p.P(`res.`, fieldName, `=this.`, fieldName)
+			p.Out()
+			p.P(`}`)
 		}
-
-		p.P(`if isLevelEnabled(`, p.s12proto.Use(), `.Level_`, getLevelValue(field).String(), `) {`)
-		p.In()
-		p.generateParseCallRow(field, fieldName, file.GoPackageName())
-		p.Out()
-		p.P(`}`)
 	}
 	p.P(`return res`)
 	p.Out()
 	p.P(`}`)
-}
-
-func (p *plugin) generateOneOfFieldRow(typeName, fieldName, fieldNameOneOf, namespace string, field *descriptor.FieldDescriptorProto) {
-	fieldTypename := strings.TrimPrefix(field.GetTypeName(), ".")
-	fieldTypename = strings.TrimPrefix(fieldTypename, namespace+".")
-
-	p.P(`if reflect.TypeOf(this.`, fieldName, `) == reflect.TypeOf(&`, typeName, `_`, fieldNameOneOf, `{}){`)
-	p.In()
-	if !hasPayloadLoggerExtensions(field) {
-		if field.IsMessage() {
-			oneOfVarName := fmt.Sprint(`oneof_`, typeName, `_`, fieldNameOneOf)
-			p.P(oneOfVarName, `:=this.`, fieldName, `.(*`, typeName, `_`, fieldNameOneOf, `)`)
-
-			p.P(`mtype := `, p.refectPkg.Use(), `.TypeOf((*foo)(nil)).Elem()`)
-			p.P(`fieldtype := `, p.refectPkg.Use(), `.TypeOf(`, oneOfVarName, `.`, fieldNameOneOf, `)`)
-
-			p.P(`if `, oneOfVarName, `!=nil && fieldtype.Implements(mtype) {`)
-			p.In()
-			p.P(oneOfVarName, `.`, fieldNameOneOf, `=`, oneOfVarName, `.`, fieldNameOneOf, `.Parse(isLevelEnabled).(*`, fieldTypename, `)`)
-			p.P(`res.`, fieldName, ` = `, oneOfVarName)
-			p.Out()
-			p.P(`}`)
-		} else {
-			p.P(`res.`, fieldName, ` = this.`, fieldName)
-		}
-	} else {
-		p.P(`if isLevelEnabled(`, p.s12proto.Use(), `.Level_`, getLevelValue(field).String(), `) {`)
-		p.In()
-		if field.IsMessage() {
-			oneOfVarName := fmt.Sprintf(`oneof_%s_%s`, typeName, fieldNameOneOf)
-			p.P(oneOfVarName, `:=this.`, fieldName, `.(*`, typeName, `_`, fieldNameOneOf, `)`)
-			p.P(`mtype := `, p.refectPkg.Use(), `.TypeOf((*foo)(nil)).Elem()`)
-			p.P(`fieldtype := `, p.refectPkg.Use(), `.TypeOf(`, oneOfVarName, `.`, fieldNameOneOf, `)`)
-
-			p.P(`if `, oneOfVarName, `!=nil && fieldtype.Implements(mtype) {`)
-			p.In()
-			p.P(oneOfVarName, `.`, fieldNameOneOf, `=`, oneOfVarName, `.`, fieldNameOneOf, `.Parse(isLevelEnabled).(*`, fieldTypename, `)`)
-			p.P(`res.`, fieldName, ` = `, oneOfVarName)
-			p.Out()
-			p.P(`}`)
-		} else {
-			p.P(`res.`, fieldName, ` = this.`, fieldName)
-		}
-		p.Out()
-		p.P(`}`)
-	}
-	p.Out()
-	p.P(`}`)
-}
-
-func (p *plugin) generateParseCallRow(field *descriptor.FieldDescriptorProto, fieldName, namespace string) {
-	fieldTypename := strings.TrimPrefix(field.GetTypeName(), ".")
-	fieldTypename = strings.TrimPrefix(fieldTypename, namespace+".")
-
-	if field.IsMessage() {
-		p.P(`mtype:=`, p.refectPkg.Use(), `.TypeOf((*foo)(nil)).Elem()`)
-		p.P(`fieldtype:=`, p.refectPkg.Use(), `.TypeOf(this.`, fieldName, `)`)
-		p.P(`if this.`, fieldName, `!=nil && fieldtype.Implements(mtype){`)
-		p.In()
-		p.P(`res.`, fieldName, `=this.`, fieldName, `.Parse(isLevelEnabled).(*`, fieldTypename, `)`)
-		p.Out()
-		p.P(`}`)
-	} else {
-		p.P(`res.`, fieldName, `=this.`, fieldName)
-	}
-
 }
 
 func getLevelValue(field *descriptor.FieldDescriptorProto) *logger.Level {
