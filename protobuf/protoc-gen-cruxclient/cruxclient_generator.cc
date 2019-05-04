8ce0d57ba345e6c6aae6e5b21b11ee5a2d01a1fa
Roger Chapman <rogchap@gmail.com>
2019-05-04 14:20:21 +1000

    All the header generation

--- protobuf/protoc-gen-cruxclient/cruxclient_generator.cc
+++ protobuf/protoc-gen-cruxclient/cruxclient_generator.cc
@@ -5,8 +5,8 @@
 #include <google/protobuf/descriptor.h>
 #include <google/protobuf/io/printer.h>
 #include <google/protobuf/io/zero_copy_stream.h>
-#include <string>
 #include <iostream>
+#include <string>
 
 using google::protobuf::Descriptor;
 using google::protobuf::FileDescriptor;
@@ -18,57 +18,45 @@ using google::protobuf::compiler::ParseGeneratorParameter;
 using google::protobuf::compiler::PluginMain;
 using google::protobuf::io::Printer;
 using google::protobuf::io::ZeroCopyOutputStream;
-
-namespace cruxclient_generator
-{
-namespace
-{
-
 using std::string;
+
+namespace cruxclient_generator {
+namespace {
 
 // The following 5 functions were copied from
 // google/protobuf/src/google/protobuf/stubs/strutil.h
 
-inline bool HasPrefixString(const string &str, const string &prefix)
-{
-  return str.size() >= prefix.size() && str.compare(0, prefix.size(), prefix) == 0;
+inline bool HasPrefixString(const string &str, const string &prefix) {
+  return str.size() >= prefix.size() &&
+         str.compare(0, prefix.size(), prefix) == 0;
 }
 
-inline string StripPrefixString(const string &str, const string &prefix)
-{
-  if (HasPrefixString(str, prefix))
-  {
+inline string StripPrefixString(const string &str, const string &prefix) {
+  if (HasPrefixString(str, prefix)) {
     return str.substr(prefix.size());
-  }
-  else
-  {
+  } else {
     return str;
   }
 }
 
-inline bool HasSuffixString(const string &str, const string &suffix)
-{
-  return str.size() >= suffix.size() && str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
+inline bool HasSuffixString(const string &str, const string &suffix) {
+  return str.size() >= suffix.size() &&
+         str.compare(str.size() - suffix.size(), suffix.size(), suffix) == 0;
 }
 
-inline string StripSuffixString(const string &str, const string &suffix)
-{
-  if (HasSuffixString(str, suffix))
-  {
+inline string StripSuffixString(const string &str, const string &suffix) {
+  if (HasSuffixString(str, suffix)) {
     return str.substr(0, str.size() - suffix.size());
-  }
-  else
-  {
+  } else {
     return str;
   }
 }
 
-void ReplaceCharacters(string *s, const char *remove, char replacewith)
-{
+void ReplaceCharacters(string *s, const char *remove, char replacewith) {
   const char *str_start = s->c_str();
   const char *str = str_start;
-  for (str = strpbrk(str, remove); str != nullptr; str = strpbrk(str + 1, remove))
-  {
+  for (str = strpbrk(str, remove); str != nullptr;
+       str = strpbrk(str + 1, remove)) {
     (*s)[str - str_start] = replacewith;
   }
 }
@@ -76,14 +64,10 @@ void ReplaceCharacters(string *s, const char *remove, char replacewith)
 // The following function was copied from
 // google/protobuf/src/google/protobuf/compiler/cpp/cpp_helpers.cc
 
-string StripProto(const string &filename)
-{
-  if (HasSuffixString(filename, ".protodevel"))
-  {
+string StripProto(const string &filename) {
+  if (HasSuffixString(filename, ".protodevel")) {
     return StripSuffixString(filename, ".protodevel");
-  }
-  else
-  {
+  } else {
     return StripSuffixString(filename, ".proto");
   }
 }
@@ -91,18 +75,13 @@ string StripProto(const string &filename)
 // The following 3 function was copied from
 // grpc/src/compiler/cpp_generator.cc
 
-string FilenameIdentifier(const string &filename)
-{
+string FilenameIdentifier(const string &filename) {
   string result;
-  for (unsigned i = 0; i < filename.size(); i++)
-  {
+  for (unsigned i = 0; i < filename.size(); i++) {
     char c = filename[i];
-    if (isalnum(c))
-    {
+    if (isalnum(c)) {
       result.push_back(c);
-    }
-    else
-    {
+    } else {
       static char hex[] = "0123456789abcdef";
       result.push_back('_');
       result.push_back(hex[(c >> 4) & 0xf]);
@@ -112,15 +91,13 @@ string FilenameIdentifier(const string &filename)
   return result;
 }
 
-inline string StringReplace(string str, const string &from, const string &to, bool replace_all)
-{
+inline string StringReplace(string str, const string &from, const string &to,
+                            bool replace_all) {
   size_t pos = 0;
 
-  do
-  {
+  do {
     pos = str.find(from, pos);
-    if (pos == string::npos)
-    {
+    if (pos == string::npos) {
       break;
     }
     str.replace(pos, from.length(), to);
@@ -130,31 +107,27 @@ inline string StringReplace(string str, const string &from, const string &to, bo
   return str;
 }
 
-inline string StringReplace(string str, const string &from, const string &to)
-{
+inline string StringReplace(string str, const string &from, const string &to) {
   return StringReplace(str, from, to, true);
 }
 
 // The following 4 function was copied from
 // grpc/src/compiler/generator_helpers.h
-inline std::vector<string> tokenize(const string &input, const string &delimiters)
-{
+inline std::vector<string> tokenize(const string &input,
+                                    const string &delimiters) {
   std::vector<string> tokens;
   size_t pos, last_pos = 0;
 
-  for (;;)
-  {
+  for (;;) {
     bool done = false;
     pos = input.find_first_of(delimiters, last_pos);
-    if (pos == string::npos)
-    {
+    if (pos == string::npos) {
       done = true;
       pos = input.length();
     }
 
     tokens.push_back(input.substr(last_pos, pos - last_pos));
-    if (done)
-      return tokens;
+    if (done) return tokens;
 
     last_pos = pos + 1;
   }
@@ -163,152 +136,182 @@ inline std::vector<string> tokenize(const string &input, const string &delimiter
 // The following 3 function was copied from
 // grpc/src/compiler/cpp_generator_helpers.h
 
-inline string DotsToColons(const string &name)
-{
+inline string DotsToColons(const string &name) {
   return StringReplace(name, ".", "::");
 }
 
-inline string DotsToUnderscores(const string &name)
-{
+inline string DotsToUnderscores(const string &name) {
   return StringReplace(name, ".", "_");
 }
 
-inline string ClassName(const Descriptor *descriptor,
-                        bool qualified)
-{
+inline string ClassName(const Descriptor *descriptor, bool qualified) {
   // Find "outer", the descriptor of the top-level message in which
   // "descriptor" is embedded.
   const Descriptor *outer = descriptor;
-  while (outer->containing_type() != NULL)
-    outer = outer->containing_type();
+  while (outer->containing_type() != NULL) outer = outer->containing_type();
 
   const string &outer_name = outer->full_name();
   string inner_name = descriptor->full_name().substr(outer_name.size());
 
-  if (qualified)
-  {
+  if (qualified) {
     return "::" + DotsToColons(outer_name) + DotsToUnderscores(inner_name);
-  }
-  else
-  {
+  } else {
     return outer->name() + DotsToUnderscores(inner_name);
   }
 }
 
-void PrintHeaderPrologue(Printer *printer, const FileDescriptor *file)
-{
+void PrintHeaderPrologue(Printer *printer, const FileDescriptor *file) {
   std::map<string, string> vars;
   vars["filename"] = file->name();
   vars["filename_identifier"] = FilenameIdentifier(file->name());
 
-  printer->Print(vars, "// Generated by the CRUX Service Client C++ plugin.\n");
-  printer->Print(vars, "// If you make any local change, they will be lost.\n");
+  printer->Print("// Generated by the CRUX Service Client C++ plugin.\n");
+  printer->Print("// If you make any local change, they will be lost.\n");
   printer->Print(vars, "// source: $filename$\n");
   printer->Print(vars, "#ifndef CRUX_CLIENT_$filename_identifier$__INCLUDED\n");
-  printer->Print(vars, "#define CRUX_CLIENT_$filename_identifier$__INCLUDED\n");
-  printer->Print(vars, "\n");
+  printer->Print(vars,
+                 "#define CRUX_CLIENT_$filename_identifier$__INCLUDED\n\n");
 }
 
-void PrintHeaderIncludes(Printer *printer, const FileDescriptor *file)
-{
+void PrintHeaderIncludes(Printer *printer, const FileDescriptor *file) {
   std::map<string, string> vars;
   vars["filename_base"] = StripProto(file->name());
 
-  printer->Print(vars, "#pragma once\n\n");
+  printer->Print("#pragma once\n\n");
   printer->Print(vars, "#include \"$filename_base$.grpc.pb.h\"\n\n");
-  printer->Print(vars, "#include <string>\n");
-  printer->Print(vars, "#include <memory>\n\n");
-  printer->Print(vars, "namespace crux {\n\n");
+  printer->Print("#include <string>\n");
+  printer->Print("#include <memory>\n\n");
+  printer->Print("namespace crux {\n\n");
 }
 
-void PrintHeaderInterfaces(Printer *printer, const FileDescriptor *file)
-{
+void PrintHeaderMethods(Printer *printer, const ServiceDescriptor *service,
+                        bool isVirtual) {
   std::map<string, string> vars;
-  for (int service_index = 0; service_index < file->service_count(); ++service_index)
-  {
+  for (int method_index = 0; method_index < service->method_count();
+       ++method_index) {
+    const MethodDescriptor *method = service->method(method_index);
+    vars["method_name"] = method->name();
+    vars["request"] = ClassName(method->input_type(), true);
+    vars["response"] = ClassName(method->output_type(), true);
+
+    if (method->client_streaming()) {
+      // [RC]: Client Steaming not supported yet
+      continue;
+    }
+
+    if (method->server_streaming()) {
+      // TODO: [RC]: Is the interface for streaming different?
+    } else {
+      if (isVirtual) {
+        printer->Print("virtual ");
+      }
+      printer->Print(vars,
+                     "$response$ $method_name$(const $request$& "
+                     "request) const");
+      if (isVirtual) {
+        printer->Print(" = 0");
+      }
+      printer->Print(";\n");
+    }
+  }
+}
+
+void PrintHeaderInterfaces(Printer *printer, const FileDescriptor *file) {
+  std::map<string, string> vars;
+  for (int service_index = 0; service_index < file->service_count();
+       ++service_index) {
     const ServiceDescriptor *service = file->service(service_index);
     vars["service_name"] = service->name();
 
     printer->Print(vars, "class $service_name$ClientInterface {\n");
-    printer->Indent();
-    printer->Print(vars, "public:\n");
+    printer->Print(" public:\n");
     printer->Indent();
     printer->Print(vars, "virtual ~$service_name$ClientInterface() {}\n");
-
-    for (int method_index = 0; method_index < service->method_count(); ++method_index)
-    {
-      const MethodDescriptor *method = service->method(method_index);
-      vars["method_name"] = method->name();
-      vars["request"] = ClassName(method->input_type(), true);
-      vars["response"] = ClassName(method->output_type(), true);
-
-      if (method->client_streaming())
-      {
-        // [RC]: Client Steaming not supported yet
-        continue;
-      }
-
-      if (method->server_streaming())
-      {
-        // TODO: [RC]: Is the interface for streaming different?
-      }
-      else
-      {
-        printer->Print(vars, "virtual $response$ $method_name$(const $request$& request) const = 0;\n");
-      }
-    }
-
+    PrintHeaderMethods(printer, service, true);
     printer->Outdent();
-    printer->Outdent();
-    printer->Print(vars, "};\n\n");
+    printer->Print("};\n\n");
   }
 }
 
-void PrintHeaderEpilogue(Printer *printer, const FileDescriptor *file)
-{
+void PrintHeaderClasses(Printer *printer, const FileDescriptor *file) {
+  std::map<string, string> vars;
+  vars["package"] = file->package();
+  if (!file->package().empty()) {
+    vars["package"].append("::");
+  }
+  for (int service_index = 0; service_index < file->service_count();
+       ++service_index) {
+    const ServiceDescriptor *service = file->service(service_index);
+    vars["service_name"] = service->name();
+
+    printer->Print(
+        vars,
+        "class $service_name$Client: public $service_name$ClientInterface {\n");
+    printer->Print(" public:\n");
+    printer->Indent();
+    printer->Print(vars,
+                   "explicit $service_name$Client(const "
+                   "std::shared_ptr<::$package$$service_name$::StubInterface>& "
+                   "stub);\n");
+    PrintHeaderMethods(printer, service, false);
+    printer->Outdent();
+    printer->Print("\n");
+
+    printer->Print(" private:\n");
+    printer->Indent();
+    printer->Print(
+        vars,
+        "std::shared_ptr<::$package$$service_name$::StubInterface> mStub;\n\n");
+    printer->Outdent();
+    printer->Print("};\n\n");
+  }
+}
+
+void PrintHeaderEpilogue(Printer *printer, const FileDescriptor *file) {
   std::map<string, string> vars;
   vars["filename"] = file->name();
   vars["filename_identifier"] = FilenameIdentifier(file->name());
 
   printer->Print(vars, "}  // namespace crux\n\n");
-  printer->Print(vars, "#endif  // GRPC_SERVICE_$filename_identifier$__INCLUDED\n");
+  printer->Print(vars,
+                 "#endif  // GRPC_SERVICE_$filename_identifier$__INCLUDED\n");
 }
 
-class Generator : public CodeGenerator
-{
-public:
+class Generator : public CodeGenerator {
+ public:
   Generator() {}
   ~Generator() override {}
 
-  bool Generate(const FileDescriptor *file, const string &parameter, GeneratorContext *context, string *error) const override
-  {
-    if (!file->service_count())
-    {
+  bool Generate(const FileDescriptor *file, const string &parameter,
+                GeneratorContext *context, string *error) const override {
+    if (!file->service_count()) {
       // No services, nothing to do.
       return true;
     }
 
     string file_name = StripProto(file->name());
 
-    std::unique_ptr<ZeroCopyOutputStream> header_output(context->Open(file_name + ".crux.client.pb.h"));
+    std::unique_ptr<ZeroCopyOutputStream> header_output(
+        context->Open(file_name + ".crux.client.pb.h"));
     Printer header_printer(header_output.get(), '$');
     PrintHeaderPrologue(&header_printer, file);
     PrintHeaderIncludes(&header_printer, file);
     PrintHeaderInterfaces(&header_printer, file);
+    PrintHeaderClasses(&header_printer, file);
     PrintHeaderEpilogue(&header_printer, file);
 
-    std::unique_ptr<ZeroCopyOutputStream> source_output(context->Open(file_name + ".crux.client.pb.cc"));
+    std::unique_ptr<ZeroCopyOutputStream> source_output(
+        context->Open(file_name + ".crux.client.pb.cc"));
     Printer source_printer(source_output.get(), '$');
 
     return true;
   }
 };
 
-} // namespace
-} // namespace cruxclient_generator
+}  // namespace
+}  // namespace cruxclient_generator
 
-int main(int argc, char *argv[])
-{
+int main(int argc, char *argv[]) {
   cruxclient_generator::Generator generator;
   PluginMain(argc, argv, &generator);
   return 0;
--- protobuf/protoc-gen-cruxclient/example/route_guide.crux.client.pb.h
+++ protobuf/protoc-gen-cruxclient/example/route_guide.crux.client.pb.h
@@ -13,11 +13,20 @@
 
 namespace crux {
 
-class RouteGuideInterface {
-  public:
-    virtual ~RouteGuideInterface() {}
+class RouteGuideClientInterface {
+ public:
+  virtual ~RouteGuideClientInterface() {}
+  virtual ::routeguide::Feature GetFeature(const ::routeguide::Point& request) const = 0;
+};
 
-    virtual ::routeguide::Feature GetFeature(const ::routeguide::Point& request) const = 0;
+class RouteGuideClient: public RouteGuideClientInterface {
+ public:
+  explicit RouteGuideClient(const std::shared_ptr<::routeguide::RouteGuide::StubInterface>& stub);
+  ::routeguide::Feature GetFeature(const ::routeguide::Point& request) const;
+
+ private:
+  std::shared_ptr<::routeguide::RouteGuide::StubInterface> mStub;
+
 };
 
 }  // namespace crux
