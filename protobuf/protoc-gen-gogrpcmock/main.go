113f630800b3a7b1eaf5d98a24fc5ee3811d021e
Alin Panaitiu <alin.panaitiu@gmail.com>
2019-11-12 18:13:54 +0200

    Update main to use already implemented gogo methods

--- protobuf/protoc-gen-gogrpcmock/main.go
+++ protobuf/protoc-gen-gogrpcmock/main.go
@@ -3,57 +3,12 @@
 package main
 
 import (
-	"io/ioutil"
-	"log"
-	"os"
-	"strings"
-
 	"github.com/SafetyCulture/s12-proto/protobuf/protoc-gen-gogrpcmock/plugin"
-	"github.com/gogo/protobuf/proto"
-	"github.com/gogo/protobuf/protoc-gen-gogo/generator"
+	"github.com/gogo/protobuf/vanity/command"
 )
 
 func main() {
-	gen := generator.New()
-	data, err := ioutil.ReadAll(os.Stdin)
-	if err != nil {
-		gen.Error(err, "gogrpcmock: reading input")
-	}
-	if err := proto.Unmarshal(data, gen.Request); err != nil {
-		gen.Error(err, "gogrpcmock: parsing input proto")
-	}
-
-	filesToGenerate := make([]string, 0, len(gen.Request.ProtoFile))
-	for _, file := range gen.Request.ProtoFile {
-		if len(file.Service) > 0 {
-			filesToGenerate = append(filesToGenerate, *file.Name)
-		}
-	}
-	gen.Request.FileToGenerate = filesToGenerate
-
-	if len(gen.Request.FileToGenerate) == 0 {
-		log.Println("gogrpcmock: no files to generate")
-		return
-	}
-
-	gen.CommandLineParameters(gen.Request.GetParameter())
-	gen.WrapTypes()
-	gen.SetPackageNames()
-	gen.BuildTypeNameMap()
-
-	gen.GeneratePlugin(plugin.New())
-
-	for i := 0; i < len(gen.Response.File); i++ {
-		gen.Response.File[i].Name = proto.String(strings.Replace(*gen.Response.File[i].Name, ".pb.go", ".mock.go", -1))
-	}
-
-	data, err = proto.Marshal(gen.Response)
-	if err != nil {
-		gen.Error(err, "gogrpcmock: failed to marshal output proto")
-	}
-
-	_, err = os.Stdout.Write(data)
-	if err != nil {
-		gen.Error(err, "gogrpcmock: failed to write output proto")
-	}
+	req := command.Read()
+	resp := command.GeneratePlugin(req, plugin.New(), ".mock.go")
+	command.Write(resp)
 }
