3e7d68a3cf84a7e3c0a29267732bdd3f0dc06698
Peter Arts <peter.arts@safetyculture.io>
2021-11-12 11:44:50 +1100

    Replace HANGUL FILLER to normal space

--- protobuf/protoc-gen-govalidator/plugin/validation_definitions.go
+++ protobuf/protoc-gen-govalidator/plugin/validation_definitions.go
@@ -86,6 +86,7 @@ var stringUnsafeReplacerMap = map[string]string{
 	`\u005C`: `\uFF3C`, //  \ : ＼  REVERSE SOLIDUS to FULLWIDTH REVERSE SOLIDUS (alternative ⧵ U+29F5)
 	`\u0060`: `\u2019`, //  ` : ’   GRAVE ACCENT to RIGHT SINGLE QUOTATION MARK
 	`\u007C`: `\uFFE8`, //  | : ￨  VERTICAL LINE to HALFWIDTH FORMS LIGHT VERTICAL
+	`\u3164`: `\u0020`, //  ㅤ :    HANGUL FILLER to normal space (HANGUL FILLER is in Letter, other so accepted by default)
 	// NOTE: when updating, also need to update UnsafeCharReplacer in validator_helpers.go (TODO PA: improve this)
 }
 
