a9d16f15f1baed001210386cb4aff55ef7b6b372
Peter Arts <77252233+sc-peter@users.noreply.github.com>
2022-02-16 15:49:39 +1100

    [SEC-1025] Improve email validation and remove govalidator dependency (#92)
    * Remove govalidator dependency
    * Consider email with trailing dot invalid
    * Validate email maximum length

--- s12/protobuf/proto/validator_helpers.go
+++ s12/protobuf/proto/validator_helpers.go
@@ -5,8 +5,6 @@ package proto
 import (
 	"regexp"
 	"strings"
-
-	"github.com/asaskevich/govalidator"
 )
 
 const (
@@ -19,6 +17,10 @@ const (
 	legacyId string = "(?i)^[0-9a-f]{8}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{4}-?[0-9a-f]{12}(-?[0-9a-f]{2,5}-?[0-9a-f]{16})?$"
 	s12Id    string = "^[a-z]*_([0-9a-fA-F]){32}$"
 	rePua    string = `[\x{E000}-\x{F8FF}]` // Private Use Codepoints in the Basic Multilingual Plane (not including planes 15, 16)
+	// govalidator package used before seems unmaintained at the moment and we needed changes to the regex so copied it from https://github.com/asaskevich/govalidator/blob/f21760c49a8d602d863493de796926d2a5c1138d/patterns.go#L77
+	// ensure all checks are performmed in IsValidEmail so we can revert this later if we want to
+	reEmail string = "^((((([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+(\\.([a-zA-Z]|\\d|[!#\\$%&'\\*\\+\\-\\/=\\?\\^_`{\\|}~]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])+)*)|((\\x22)((((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(([\\x01-\\x08\\x0b\\x0c\\x0e-\\x1f\\x7f]|\\x21|[\\x23-\\x5b]|[\\x5d-\\x7e]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(\\([\\x01-\\x09\\x0b\\x0c\\x0d-\\x7f]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}]))))*(((\\x20|\\x09)*(\\x0d\\x0a))?(\\x20|\\x09)+)?(\\x22)))){1,14}@((([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|\\.|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|\\d|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))\\.)+(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])|(([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])([a-zA-Z]|\\d|-|_|~|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])*([a-zA-Z]|[\\x{00A0}-\\x{D7FF}\\x{F900}-\\x{FDCF}\\x{FDF0}-\\x{FFEF}])))$"
+	reUUID4 string = "^[0-9a-f]{8}-[0-9a-f]{4}-4[0-9a-f]{3}-[89ab][0-9a-f]{3}-[0-9a-f]{12}$"
 )
 
 var (
@@ -29,6 +31,8 @@ var (
 	SymbolCharReplacer          = strings.NewReplacer("\u00A0", "\u0020", "\u1680", "\u0020", "\u2000", "\u0020", "\u2001", "\u0020", "\u2002", "\u0020", "\u2003", "\u0020", "\u2004", "\u0020", "\u2005", "\u0020", "\u2006", "\u0020", "\u2007", "\u0020", "\u2008", "\u0020", "\u2009", "\u0020", "\u200A", "\u0020", "\u202F", "\u0020", "\u205F", "\u0020", "\u3000", "\u0020", "\u200C", "", "\u200D", "", "\uFEFF", "", "\u2014", "\u2013", "\u2018", "\u2019", "\u3002", "\u002E", "\uFF0C", "\u002C", "\uFF1A", "\u003A", "\u0009", "\u0020", "\u000A", "\u0020", "\u000D", "\u0020")
 	SymbolCharReplacerMultiline = strings.NewReplacer("\u00A0", "\u0020", "\u1680", "\u0020", "\u2000", "\u0020", "\u2001", "\u0020", "\u2002", "\u0020", "\u2003", "\u0020", "\u2004", "\u0020", "\u2005", "\u0020", "\u2006", "\u0020", "\u2007", "\u0020", "\u2008", "\u0020", "\u2009", "\u0020", "\u200A", "\u0020", "\u202F", "\u0020", "\u205F", "\u0020", "\u3000", "\u0020", "\u200C", "", "\u200D", "", "\uFEFF", "", "\u2014", "\u2013", "\u2018", "\u2019", "\u3002", "\u002E", "\uFF0C", "\u002C", "\uFF1A", "\u003A", "\u0009", "\u0020", "\u000D", "\u0020")
 	RegexPua                    = regexp.MustCompile(rePua)
+	RegexEmail                  = regexp.MustCompile(reEmail)
+	RegexUUID4                  = regexp.MustCompile(reUUID4)
 )
 
 // IsUUID checks if the string is a UUID (version 3, 4 or 5).
@@ -38,7 +42,8 @@ func IsUUID(str string) bool {
 
 // IsUUIDv4 checks if the string is a UUIDv4
 func IsUUIDv4(str string) bool {
-	return govalidator.IsUUIDv4(str)
+	return RegexUUID4.MatchString(str)
+
 }
 
 // IsLegacyID A legacyId does not contain the document prefix; the prefix is accounted for in the service implementation code
@@ -54,11 +59,14 @@ func IsS12ID(str string) bool {
 
 // IsValidEmail checks if an email address is a valid RFC 5322 address
 func IsValidEmail(str string, checkDomain bool) bool {
-	valid := govalidator.IsEmail(str)
-	if valid && checkDomain {
-		return govalidator.IsExistingEmail(str)
+	// ignore checkDomain for now
+	// do the length check first for performance reasons
+	if len(str) > 321 {
+		// check length; max length is 64 chars for user part + @ + 256 for domain = 321
+		// this is not a perfect check against RFC, but close enough for our purposes atm
+		return false
 	}
-	return valid
+	return RegexEmail.MatchString(str)
 }
 
 type Validator interface {
