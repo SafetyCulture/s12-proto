bfc0b81460d70268a298a544f7900128159c3f60
Peter Arts <peter.arts@safetyculture.io>
2022-07-06 16:49:13 +1000

    Bump validator version

--- protobuf/protoc-gen-govalidator/plugin/plugin.go
+++ protobuf/protoc-gen-govalidator/plugin/plugin.go
@@ -39,7 +39,7 @@ var regexGeneratedFile *protogen.GeneratedFile
 var regexHashLib = make(map[string]struct{})
 
 // Validator plugin version
-var validatorVersion = "v2.3.0"
+var validatorVersion = "v2.4.0"
 
 // Write a preamble in the auto generated files
 func genGeneratedHeader(gen *protogen.Plugin, g *protogen.GeneratedFile, f *protogen.File) {
