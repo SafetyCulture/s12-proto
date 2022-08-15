5625d85140049b52ed1e9bc1d268e518184ced7b
Mick Stanciu <mick.stanciu@safetyculture.io>
2022-08-15 16:36:55 +1000

    INSPECT-5932 bump version

--- protobuf/protoc-gen-govalidator/plugin/plugin.go
+++ protobuf/protoc-gen-govalidator/plugin/plugin.go
@@ -40,7 +40,7 @@ var regexGeneratedFile *protogen.GeneratedFile
 var regexHashLib = make(map[string]struct{})
 
 // Validator plugin version
-var validatorVersion = "v2.5.0"
+var validatorVersion = "v2.5.1"
 
 // Write a preamble in the auto generated files
 func genGeneratedHeader(gen *protogen.Plugin, g *protogen.GeneratedFile, f *protogen.File) {
