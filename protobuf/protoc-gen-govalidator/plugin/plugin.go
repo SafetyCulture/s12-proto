4d154d0d815e338c86e333f92193fdd81019282a
Lachlan Robertson <lachlanrobertson@safetyculture.io>
2024-09-17 16:28:03 +1000

    Bump version in plugin.go

--- protobuf/protoc-gen-govalidator/plugin/plugin.go
+++ protobuf/protoc-gen-govalidator/plugin/plugin.go
@@ -41,7 +41,7 @@ var regexGeneratedFile *protogen.GeneratedFile
 var regexHashLib = make(map[string]struct{})
 
 // Validator plugin version
-var validatorVersion = "v2.6.0"
+var validatorVersion = "v2.7.0"
 
 // Write a preamble in the auto generated files
 func genGeneratedHeader(gen *protogen.Plugin, g *protogen.GeneratedFile, f *protogen.File) {
